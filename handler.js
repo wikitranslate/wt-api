a026b3448447e7d838c7d6a3d74d4f5de68379b4
Nathan Gaberel <nathang@theodo.co.uk>
2016-10-18 09:43:09 +0100

    Add /langs endpoint.

--- handler.js
+++ handler.js
@@ -1,9 +1,15 @@
 const wt = require('wikitranslate');
 
-module.exports.wikitranslate = (event, context, cb) => {
+module.exports.translate = (event, context, cb) => {
   const params = event.path;
 
   return wt(params.input, params.fromLang, params.toLang)
   .then(translation => cb(null, translation))
   .catch(err => cb(err));
+};
+
+module.exports.langs = (event, context, cb) => {
+  cb(null, {
+    langs: wt.langs
+  });
 };
