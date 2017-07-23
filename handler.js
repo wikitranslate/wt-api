9d8a89aec379622aedd2bca4c801d7dc558e8363
Nathan Gaberel <nathang@theodo.co.uk>
2017-07-23 14:08:20 +0100

    Use standard

--- handler.js
+++ handler.js
@@ -1,15 +1,15 @@
-const wt = require('wikitranslate');
+const wt = require('wikitranslate')
 
 module.exports.translate = (event, context, cb) => {
-  const params = event.path;
+  const params = event.path
 
   return wt(params.input, params.fromLang, params.toLang)
   .then(translation => cb(null, translation))
-  .catch(err => cb(err));
-};
+  .catch(err => cb(err))
+}
 
 module.exports.langs = (event, context, cb) => {
   cb(null, {
     langs: wt.langs
-  });
-};
+  })
+}
